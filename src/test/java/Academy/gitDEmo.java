3fdbc7939a9cae541ee1631d4adae0491746060c
Malav Panchal <malav.4395@gmail.com>
2020-05-20 11:28:09 -0400

    ddd

--- src/test/java/Academy/gitDEmo.java
+++ src/test/java/Academy/gitDEmo.java
@@ -13,7 +13,17 @@ public class gitDEmo {
 	{
 		System.out.println("example 22");
 	}
-
+	
+	@Test
+	public void test13()
+	{
+		System.out.println("example 23");
+	}
+	@Test
+	public void test14()
+	{
+		System.out.println("example 24");
+	}
 
 	
 	@AfterTest
