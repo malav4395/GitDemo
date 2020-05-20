1c2306aa08d3d95388d65a4ff25ffcbed664c243
Malav Panchal <malav.4395@gmail.com>
2020-05-20 11:12:43 -0400

    gitDemo commit from develop2

--- src/test/java/Academy/gitDEmo.java
+++ src/test/java/Academy/gitDEmo.java
@@ -13,16 +13,7 @@ public class gitDEmo {
 	{
 		System.out.println("example 22");
 	}
-	@Test
-	public void test13()
-	{
-		System.out.println("example 33");
-	}
-	@Test
-	public void test14()
-	{
-		System.out.println("example 44");
-	}
+
 
 	
 	@AfterTest
