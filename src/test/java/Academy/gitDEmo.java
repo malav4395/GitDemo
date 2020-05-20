1b72fe46624485cff117377a66dd9727392027e1
Malav Panchal <malav.4395@gmail.com>
2020-05-20 11:10:05 -0400

    gitDemo commit

--- src/test/java/Academy/gitDEmo.java
+++ src/test/java/Academy/gitDEmo.java
@@ -7,6 +7,22 @@ public class gitDEmo {
 	{
 		System.out.println("example gggit");
 	}
+	
+	@Test
+	public void test12()
+	{
+		System.out.println("example 22");
+	}
+	@Test
+	public void test13()
+	{
+		System.out.println("example 33");
+	}
+	@Test
+	public void test14()
+	{
+		System.out.println("example 44");
+	}
 
 
 }
